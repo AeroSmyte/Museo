8cf294b11b9fc5ccd36008bfbffe6d40a61a9fe7
Lexi McQueen <alexismcqueen01@gmail.com>
2024-05-16 21:48:58 -0400

    Initial Commit

---
+++ MuseoPin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MuseoPin
+//
+//  Created by Lexi McQueen on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MuseoPin/MuseoPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuseoPinApp.swift
+//  MuseoPin
+//
+//  Created by Lexi McQueen on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct MuseoPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
